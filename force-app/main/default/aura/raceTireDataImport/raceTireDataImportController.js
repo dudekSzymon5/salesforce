e59cf25b8cd381ce90c4ddcb6e8dabb66716bfbf
Szymon Dudek <szymon.dudek@britenet.com.pl>
2026-05-29 11:18:18 +0200

    feat: add Aura CSV import component for race data

---
+++ force-app/main/default/aura/raceTireDataImport/raceTireDataImportController.js
@@ -0,0 +1,32 @@
+({
+    handleFileChange: function(component, fileChangeEvent, helper) {
+        var file = fileChangeEvent.getSource().get('v.files')[0];
+        if (!file) {
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function(loadEvent) {
+            helper.parseCSV(component, loadEvent.target.result);
+        };
+        reader.readAsText(file);
+    },
+
+    handleImport: function(component, importEvent, helper) {
+        helper.importRecords(component);
+    },
+
+    handleGoToRecords: function(component, goToRecordsEvent, helper) {
+        var navigationEvent = $A.get('e.force:navigateToObjectHome');
+        navigationEvent.setParams({ scope: 'Race_Tire_Data__c' });
+        navigationEvent.fire();
+    },
+
+    handleReset: function(component, resetEvent, helper) {
+        component.set('v.parsedRows', []);
+        component.set('v.importResults', []);
+        component.set('v.isParsed', false);
+        component.set('v.showResults', false);
+        component.set('v.validCount', 0);
+        component.set('v.errorCount', 0);
+    }
+})
