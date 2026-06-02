b77c2db2631f785c68e90406a4175066c7dec517
Szymon Dudek <szymon.dudek@britenet.com.pl>
2026-06-02 15:50:25 +0200

    feat: add pagination and filter constants to race tire import

--- force-app/main/default/aura/raceTireDataImport/raceTireDataImportController.js
+++ force-app/main/default/aura/raceTireDataImport/raceTireDataImportController.js
@@ -36,25 +36,47 @@
     },
 
     handleFilterAll: function(component, event, helper) {
-        helper.applyFilter(component, 'all');
+        helper.applyFilter(component, helper.FILTER.ALL);
     },
 
     handleFilterValid: function(component, event, helper) {
-        helper.applyFilter(component, 'valid');
+        helper.applyFilter(component, helper.FILTER.VALID);
     },
 
     handleFilterInvalid: function(component, event, helper) {
-        helper.applyFilter(component, 'invalid');
+        helper.applyFilter(component, helper.FILTER.INVALID);
+    },
+
+    handlePrevPage: function(component, event, helper) {
+        var currentPage = component.get('v.currentPage');
+        if (currentPage > 1) {
+            component.set('v.currentPage', currentPage - 1);
+            helper.applyPagination(component);
+        }
+    },
+
+    handleNextPage: function(component, event, helper) {
+        var currentPage = component.get('v.currentPage');
+        var totalPages = component.get('v.totalPages');
+        if (currentPage < totalPages) {
+            component.set('v.currentPage', currentPage + 1);
+            helper.applyPagination(component);
+        }
     },
 
     handleReset: function(component, resetEvent, helper) {
         component.set('v.parsedRows', []);
         component.set('v.filteredRows', []);
+        component.set('v.pagedRows', []);
         component.set('v.importResults', []);
         component.set('v.isParsed', false);
         component.set('v.showResults', false);
         component.set('v.validCount', 0);
         component.set('v.errorCount', 0);
-        component.set('v.filter', 'all');
+        component.set('v.filter', helper.FILTER.ALL);
+        component.set('v.currentPage', 1);
+        component.set('v.totalPages', 0);
+        component.set('v.isFirstPage', true);
+        component.set('v.isLastPage', true);
     }
 })
